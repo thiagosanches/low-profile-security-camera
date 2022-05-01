1f0e1fb58f4fb14d5ac261bf4e142431e994fba2
Thiago Sanches <tsigwt@gmail.com>
2022-05-01 09:46:51 -0300

    Update the path

--- very-humble-security-camera.sh
+++ very-humble-security-camera.sh
@@ -5,10 +5,10 @@ BOT_TOKEN="$2"
 
 while true
 do
-    curl "http://$CAMERA_IP/cam-hi.jpg" -o "a.jpg" --silent
+    curl "http://$CAMERA_IP/jpg" -o "a.jpg" --silent
     sleep 1
 
-    curl "http://$CAMERA_IP/cam-hi.jpg" -o "b.jpg" --silent
+    curl "http://$CAMERA_IP/jpg" -o "b.jpg" --silent
 
     RESULT=$(compare -metric RMSE -subimage-search -fuzz 20% a.jpg b.jpg x.jpg 2>&1)
     THRESHOLD=$(echo $RESULT | grep -o -E "[0-9]*\.?" | head -n1 | tr "." " ")
