4eae6a770f31bb469b405a47af93361f4a6a62fe
Thiago Sanches <tsigwt@gmail.com>
2022-05-01 09:42:48 -0300

    Add variable

--- very-humble-security-camera.sh
+++ very-humble-security-camera.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
+CAMERA_IP="$1"
+BOT_TOKEN="$2"
+
 while true
 do
-    curl "http://YOUR_CAMERA_IP/cam-hi.jpg" -o "a.jpg" --silent
+    curl "http://$CAMERA_IP/cam-hi.jpg" -o "a.jpg" --silent
     sleep 1
 
-    curl "http://YOUR_CAMERA_IP/cam-hi.jpg" -o "b.jpg" --silent
+    curl "http://$CAMERA_IP/cam-hi.jpg" -o "b.jpg" --silent
 
     RESULT=$(compare -metric RMSE -subimage-search -fuzz 20% a.jpg b.jpg x.jpg 2>&1)
     THRESHOLD=$(echo $RESULT | grep -o -E "[0-9]*\.?" | head -n1 | tr "." " ")
@@ -15,9 +18,9 @@ do
 
     if [[ $THRESHOLD -gt 3000 ]]
     then
-        curl "https://api.telegram.org/BOT_TOKEN_GOES_HERE/sendMessage?chat_id=-CHAT_ID_GOES_HERE&text=Motion%20detected!";
-        curl -X POST "https://api.telegram.org/BOT_TOKEN_GOES_HERE/sendPhoto?chat_id=-CHAT_ID_GOES_HERE" -F photo=@"a.jpg"
-        curl -X POST "https://api.telegram.org/BOT_TOKEN_GOES_HERE/sendPhoto?chat_id=-CHAT_ID_GOES_HERE" -F photo=@"b.jpg"
-        curl -X POST "https://api.telegram.org/BOT_TOKEN_GOES_HERE/sendPhoto?chat_id=-CHAT_ID_GOES_HERE" -F photo=@"x.jpg"
+        curl "https://api.telegram.org/$BOT_TOKEN/sendMessage?chat_id=-CHAT_ID_GOES_HERE&text=Motion%20detected!";
+        curl -X POST "https://api.telegram.org/$BOT_TOKEN/sendPhoto?chat_id=-CHAT_ID_GOES_HERE" -F photo=@"a.jpg"
+        curl -X POST "https://api.telegram.org/$BOT_TOKEN/sendPhoto?chat_id=-CHAT_ID_GOES_HERE" -F photo=@"b.jpg"
+        curl -X POST "https://api.telegram.org/$BOT_TOKEN/sendPhoto?chat_id=-CHAT_ID_GOES_HERE" -F photo=@"x.jpg"
     fi
 done
